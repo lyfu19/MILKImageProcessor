a6ccebe542984f60b8ef880b353e27ae389197b4
Yongfu Lin <alfielin0709@gmail.com>
2025-10-20 23:42:16 +1300

    Initial Commit

---
+++ MILKImageProcessor/MILKImageProcessorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MILKImageProcessorApp.swift
+//  MILKImageProcessor
+//
+//  Created by 阿福 on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MILKImageProcessorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
